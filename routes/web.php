e25959c916db09eba07e2c35ea6f496943661699
jeffesonsiqueira <60291325+jeffesonsiqueira@users.noreply.github.com>
2020-03-17 21:51:44 -0300

    teste upload

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,19 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'sigava', 'middleware' => ['auth']], function () {
     Route::get('/', 'AppController@index')->name("main");
     Route::get('/minor', 'AppController@minor')->name("minor");
+    Route::resource('/imovel', 'PropertyController');
+    Route::resource('/contato', 'ContactController');
+    Route::resource('/acabamento', 'AcabamentoController');
+    Route::resource('/conservacao', 'ConservacaoController');
+
+    Route::get('/parametros', 'AppController@parametro')->name('parametros');
+    Route::get('/parametros/acabamento', 'AppController@acabamento')->name('acabamento');
+
+    Route::any('/imovel/step-2/validate/{id}', 'PropertyController@storeMaintenance')->name('storeStep2');
+    Route::any('/imovel/step-2/cadastrar/{id}', 'PropertyController@createMaintenance')->name('createStep2');
+
+    Route::any('/imovel/step-3/validate/{id}', 'PropertyController@storePlus')->name('storeStep3');
+    Route::any('/imovel/step-3/cadastrar/{id}', 'PropertyController@createPlus')->name('createStep3');
 });
+
+//Teste subindo atualizacoes
